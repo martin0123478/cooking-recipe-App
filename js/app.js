97877473c51b7424091723333f00faa57f97515c
Martin Aguilar <34758911+martin0123478@users.noreply.github.com>
2023-01-11 11:39:23 -0600

    obtener platillos de cada categoria

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 function iniciarApp(){
     const selectCategorias = document.querySelector('#categorias')
+    selectCategorias.addEventListener('change',seleccionarCategoria)
 
     obtenerCategorias()
    function obtenerCategorias(){
@@ -19,6 +20,14 @@ function iniciarApp(){
           
            
         })
+   }
+
+   function seleccionarCategoria(e){
+    const categoria =  e.target.value
+     const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`
+     fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => console.log(resultado))
    }
 }
 
